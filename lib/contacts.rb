c01a98bf1131e80253107027457d49ff4dfe74ee
Aaron Parkening <aaron.parkening@gmail.com>
2019-04-22 16:25:01 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -22,5 +22,14 @@ def remove_strawberry(contacts)
   # Iterative way: 
   contacts["Freddy Mercury"][:favorite_ice_cream_flavors].delete_if {|flavor| flavor == "strawberry" }
   
+  # Longest way:
+  # contacts.each do |person, contact_details_hash|
+  #   contact_details_hash.each do |attribute, data|
+  #     if attribute == :favorite_ice_cream_flavors
+  #       data.delete_if {|ice_cream| ice_cream == "strawberry"}
+  #     end
+  #   end
+  # end
+  
   contacts
 end
